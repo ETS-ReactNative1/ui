2f6b3971f814673ea30f65b6b0d6f0f8f9b670ba
Tomislav Arambasic <tomislav.arambasic@five.agency>
2022-05-04 12:59:29 +0200

    Added left hand side animation

--- components/CategoryPicker/Category.js
+++ components/CategoryPicker/Category.js
@@ -1,16 +1,21 @@
 import React, { useMemo } from 'react';
+import { Animated } from 'react-native';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { connectStyle } from '@shoutem/theme';
-import { Text } from '../Text';
 import { TouchableOpacity } from '../TouchableOpacity';
+import { View } from '../View';
 
-function Category({ category, style, isSelected, onPress }) {
-  const textStyle = useMemo(
-    () => [style.category, !isSelected && style.selectedCategory],
-
-    [isSelected, style.category, style.selectedCategory],
-  );
-
+function Category({
+  category,
+  isSelected,
+  index,
+  scrollX,
+  categoryDimensions,
+  saveCategoryDimensions,
+  style,
+  onPress,
+}) {
   function handlePress() {
     if (isSelected) {
       return;
@@ -21,10 +26,75 @@ function Category({ category, style, isSelected, onPress }) {
     }
   }
 
+  function onLayoutChange(event) {
+    const { width } = event.nativeEvent.layout;
+
+    saveCategoryDimensions(prevDimensions => ({
+      ...prevDimensions,
+      [index]: width,
+    }));
+  }
+
+  // Width of all items before current one
+  // Default 1 - because we want to start to interpolate first item when scroll is moved, keeping
+  // transorm: 1 until it isn't
+  const requiredXoffset = useMemo(() => {
+    return _.reduce(
+      _.keys(categoryDimensions),
+      (res, categoryIndex) => {
+        if (_.toInteger(categoryIndex) < index) {
+          return res + _.toInteger(categoryDimensions[categoryIndex]);
+        }
+
+        return res;
+      },
+      1,
+    );
+  }, [categoryDimensions, index]);
+  const currentItemWidth = useMemo(() => categoryDimensions[index] || 0, [
+    categoryDimensions,
+    index,
+  ]); // 0 because of NaN error when multiplying with 0.5
+
+  const inputRange = [
+    -1, // Keep scale:1 if user tries to scroll in reverse direction & list is position: 0
+    0,
+    requiredXoffset,
+    requiredXoffset + 4,
+    requiredXoffset + 4 + currentItemWidth * 0.5,
+    requiredXoffset + 20 + currentItemWidth,
+  ];
+
+  const scale = scrollX.interpolate({
+    inputRange,
+    outputRange: [1, 1, 1, 0.9, 0.6, 0],
+  });
+  const opacity = scrollX.interpolate({
+    inputRange,
+    outputRange: [1, 1, 1, 0.9, 0.6, 0],
+  });
+  const resolvedStyle = useMemo(
+    () =>
+      categoryDimensions[index] ? { opacity, transform: [{ scale }] } : {},
+    [categoryDimensions, index, opacity, scale],
+  );
+
+  const textStyle = useMemo(
+    () => [
+      resolvedStyle,
+      style.category,
+      !isSelected && style.selectedCategory,
+    ],
+
+    [resolvedStyle, isSelected, style.category, style.selectedCategory],
+  );
+
   return (
-    <TouchableOpacity onPress={handlePress} style={style.container}>
-      <Text style={textStyle}>{category.name}</Text>
-    </TouchableOpacity>
+    <View onLayout={onLayoutChange}>
+      <TouchableOpacity onPress={handlePress} style={style.container}>
+        <Animated.Text style={textStyle}>{category.name}</Animated.Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -32,6 +102,10 @@ Category.propTypes = {
   category: PropTypes.shape({
     name: PropTypes.string.isRequired,
   }).isRequired,
+  categoryDimensions: PropTypes.object.isRequired,
+  index: PropTypes.number.isRequired,
+  saveCategoryDimensions: PropTypes.func.isRequired,
+  scrollX: PropTypes.number.isRequired,
   isSelected: PropTypes.bool,
   style: PropTypes.object,
   onPress: PropTypes.func,
--- components/CategoryPicker/CategoryPicker.js
+++ components/CategoryPicker/CategoryPicker.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Animated, FlatList } from 'react-native';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
@@ -18,63 +18,44 @@ export function CategoryPicker({
   style,
   selectedCategory,
 }) {
-  const listRef = useRef();
   const scrollX = useRef(new Animated.Value(0)).current;
-
-  // useEffect(() => {
-  //   // Problem je jer se postavi ref na prvom renderu - ali nista onda ne triggera re-render
-  //   console.log(listRef);
-  //   listRef?.current?.scrollToOffset({ animated: true, offset: 100 });
-  // }, [listRef]);
-
-  // useEffect(() => {
-  //   console.log(listRef);
-  //   _.delay(
-  //     () => listRef?.current?.scrollToOffset({ animated: true, offset: 0 }),
-  //     200,
-  //   );
-  // }, [listRef]);
+  const [categoryDimensions, setCategoryDimensions] = useState({});
 
   const renderItem = useCallback(
     ({ index, item: category }) => {
-      // console.log(index)
-
-      const inputRange = [
-        -1, 0, 10 * index, 10 * (index + 2)
-      ];
-      const scale = scrollX.interpolate({inputRange, outputRange: [1,1,1,0]})
-      console.log(scrollX)
-
       return (
-        <Animated.View style={{transform: [{scale}]}}>
-          <Category
-            category={category}
-            key={category.id}
-            onPress={onCategorySelected}
-            isSelected={selectedCategory.id === category.id}
-          />
-      </Animated.View>
-    )},
-    [selectedCategory.id, onCategorySelected]
+        <Category
+          index={index}
+          category={category}
+          onPress={onCategorySelected}
+          isSelected={selectedCategory.id === category.id}
+          scrollX={scrollX}
+          categoryDimensions={categoryDimensions}
+          saveCategoryDimensions={setCategoryDimensions}
+        />
+      );
+    },
+    [selectedCategory.id, onCategorySelected, categoryDimensions],
   );
 
   if (_.size(categories) < 2) {
     return null;
   }
 
-  // console.log('render', listRef);
 
   return (
     <View style={style.container}>
       <Animated.FlatList
         data={categories}
         renderItem={renderItem}
-        ref={listRef}
         horizontal
         scrollToOverflowEnabled
         contentContainerStyle={style.listContainer}
         showsHorizontalScrollIndicator={false}
-        onScroll={Animated.event([{nativeEvent: {contentOffset: {x: scrollX}}}], { useNativeDriver: true})}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true },
+        )}
       />
     </View>
   );
@@ -94,4 +75,6 @@ CategoryPicker.defaultProps = {
   onCategorySelected: undefined,
 };
 
-export default React.memo(connectStyle('shoutem.ui.CategoryPicker')(CategoryPicker));
+export default React.memo(
+  connectStyle('shoutem.ui.CategoryPicker')(CategoryPicker),
+);
